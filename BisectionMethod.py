53c73344d134781a570de3b4b90f8d84ea4332c9
azizullahkhan16 <aziz.bin.aman16@gmail.com>
2023-09-04 21:06:45 +0500

    Simple Bisection method implemented

--- BisectionMethod.py
+++ BisectionMethod.py
@@ -1,1 +1,27 @@
-print("Hello world!")
+def bisection_method(func, a, b, tol):
+    i = 1
+    f_a = func(a)
+
+    if f_a * func(b) >= 0:
+        raise ValueError("The interval does not contain a root to the given function")
+
+    while True:
+        p = a + (b - a) / 2
+        f_p = func(p)
+
+        if f_p == 0 or (b - a) / 2 < tol:
+            return p, i
+
+        i += 1
+        if f_p * f_a < 0:
+            b = p
+        else:
+            a = p
+
+
+def quadratic(x):
+    return x**2 - 6 * x + 4
+
+
+result = bisection_method(quadratic, 0, 4, 0.0002)
+print(f"Approximate root: {result[0]:.4f}, Iterations: {result[1]}")
